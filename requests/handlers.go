1acb41c0b2f199aca2c897f8b30fd4f31b484fe7
Vilius Paliokas <vilpal@bitbucket.org>
2019-10-24 12:14:09 +0300

    Implement get  all trips requests by ID

--- requests/handlers.go
+++ requests/handlers.go
@@ -96,7 +96,85 @@ func GetUserRequests(w http.ResponseWriter, r *http.Request) {
 
 func GetTripRequests(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	id := mux.Vars(r)["id"]
+	intID, err := strconv.Atoi(id)
+	if err != nil || intID < 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	query := `
+	SELECT 
+	requests.id_REQUEST, requests.submit_date, requests.info,
+	request_statuses.id_REQUEST_STATUS, request_statuses.name,
+	trips.id_TRIP,trips.create_date,trips.departure_date,trips.cost_per_person,trips.space,trips.info,
+	dep_city.id_CITY as id_CITY_dep, dep_city.name as name_dep,
+	dest_city.id_CITY as id_CITY_dest, dest_city.name as name_des,
+	people.id_PERSON, people.first_name, people.last_name, people.phone_number, people.photo_URL, people.isDeleted
+	FROM requests 
+	INNER JOIN request_statuses
+	ON request_statuses.id_REQUEST_STATUS = requests.request_status
+	INNER JOIN trips
+	ON trips.id_TRIP = requests.fk_TRIP
+	INNER JOIN CITIES as dep_city
+	ON dep_city.id_CITY=fk_departure_CITY
+	INNER JOIN CITIES as dest_city
+	ON dest_city.id_CITY=fk_destination_CITY
+	INNER JOIN people
+	ON people.id_PERSON = requests.fk_PERSON
+    WHERE requests.fk_TRIP=? LIMIT ?,?
+	`
+	count, _ := strconv.Atoi(r.FormValue("limit"))
+	start, _ := strconv.Atoi(r.FormValue("offset"))
 
+	if count == 0 && start == 0 {
+		count = 10
+		start = 0
+	}
+	if count <= 0 || start < 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	result, err := util.DB.Query(query, id, start, count)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	var requests []Request
+	var trip Trip
+	for result.Next() {
+		var user User
+		var departure City
+		var destination City
+		var request Request
+		var requestStatus Status
+		isDeleted := 0
+		err := result.Scan(
+			&request.RequestID, &request.SubmitDate, &request.Info,
+			&requestStatus.StatusID, &requestStatus.Name,
+			&trip.TripID, &trip.CreationDate, &trip.DepartureDate, &trip.CostPerPerson, &trip.Space, &trip.Info,
+			&departure.CityID, &departure.Name,
+			&destination.CityID, &destination.Name,
+			&user.UserID, &user.FName, &user.LName, &user.Phone, &user.PhotoURL, &isDeleted) //Last parameter is 'isDeleted'
+		if err != nil {
+			fmt.Printf("Error: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		trip.DepartureCity = departure
+		trip.DestinationCity = destination
+		request.Requester = &user
+		requests = append(requests, request)
+	}
+	trip.Requests = requests
+	requestsJSON, err := json.Marshal(trip)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, string(requestsJSON))
 }
 
 func DeleteRequestByID(w http.ResponseWriter, r *http.Request) {
--- requests/request.go
+++ requests/request.go
@@ -1,20 +1,21 @@
 package requests
 
 type Trip struct {
-	TripID          int    `json:"tripID"`
-	CreationDate    string `json:"creation_date"`
-	DepartureDate   string `json:"departure_date"`
-	CostPerPerson   int    `json:"cost"`
-	Space           int    `json:"space"`
-	Info            string `json:"info"`
-	DepartureCity   City   `json:"departure_city"`
-	DestinationCity City   `json:"destination_city"`
-	TripOwner       *User  `json:"user,omitempty"`
+	TripID          int       `json:"tripID"`
+	CreationDate    string    `json:"creation_date"`
+	DepartureDate   string    `json:"departure_date"`
+	CostPerPerson   int       `json:"cost"`
+	Space           int       `json:"space"`
+	Info            string    `json:"info"`
+	DepartureCity   City      `json:"departure_city"`
+	DestinationCity City      `json:"destination_city"`
+	TripOwner       *User     `json:"user,omitempty"`
+	Requests        []Request `json:"requests,omitempty"`
 }
 
 type Status struct {
 	StatusID int    `json:"status_ID"`
-	Name     string `json:"name`
+	Name     string `json:"name"`
 }
 
 type City struct {
@@ -35,5 +36,5 @@ type Request struct {
 	SubmitDate string `json:"submit_date"`
 	Info       string `json:"info"`
 	Requester  *User  `json:"request_user,omitempty"`
-	Trip       *Trip  `json:"Trip"`
+	Trip       *Trip  `json:"trip,omitempty"`
 }
