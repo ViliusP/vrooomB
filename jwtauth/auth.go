1f5ef57b8e0fe3dbbf579e4ed783242d637f5e29
Vilius Paliokas <vilpal@bitbucket.org>
2019-12-07 01:16:57 +0200

    Bug fix: empty password and email authorization

--- jwtauth/auth.go
+++ jwtauth/auth.go
@@ -56,6 +56,8 @@ func init() {
 func SignIn(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var creds Credentials
+	var user User
+	exists := false
 	// Get the JSON body and decode into credentials
 	err := json.NewDecoder(r.Body).Decode(&creds)
 	if err != nil {
@@ -63,16 +65,17 @@ func SignIn(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	q := `SELECT id_PERSON, email, password, isDeleted FROM people WHERE email = ?`
-	result, err := util.DB.Query(q, creds.Email)
+	q := `SELECT id_PERSON, email, password, isDeleted FROM people WHERE email = ? AND password=?`
+	result, err := util.DB.Query(q, creds.Email, creds.Password)
 	if err != nil {
 		fmt.Printf("Error: %s", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	// Get the expected password from our in memory map
-	var user User
+
 	for result.Next() {
+		exists = true
 		err := result.Scan(&user.id, &user.email, &user.password, &user.isDeleted)
 		if err != nil {
 			fmt.Printf("Error: %s", err)
@@ -84,7 +87,7 @@ func SignIn(w http.ResponseWriter, r *http.Request) {
 	// If a password exists for the given user
 	// AND, if it is the same as the password we received, the we can move ahead
 	// if NOT, then we return an "Unauthorized" status
-	if user.password != creds.Password || user.isDeleted == 1 {
+	if user.password != creds.Password || user.isDeleted == 1 || !exists {
 		w.WriteHeader(http.StatusUnauthorized)
 		return
 	}
