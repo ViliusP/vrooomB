4804705c7c345a6b5f156f14fa6383ed4d07dcc3
Vilius Paliokas <vilpal@bitbucket.org>
2019-12-07 01:09:22 +0200

    CORS fx

--- routes/router.go
+++ routes/router.go
@@ -14,15 +14,25 @@ func NewRouter() *mux.Router {
 
 	router := mux.NewRouter()
 	for _, route := range composeAllRoutes() {
+		var fakeHandler http.Handler
 		var handler http.Handler
-
+		fakeRoute := Route{
+			"CORS",
+			http.MethodOptions,
+			route.Pattern,
+			corsHandler,
+			[]string{},
+		}
+		fakeHandler = fakeRoute.HandlerFunc
 		handler = route.HandlerFunc
 		if route.Pattern == "/signin" {
-			handler = util.Logger(handler, route.Name)
+			handler = util.Logger(AllowCORSHeader(handler), route.Name)
+			fakeHandler = util.Logger(AllowCORSHeader(fakeHandler), fakeRoute.Name)
 		}
 
 		if route.Pattern != "/signin" {
-			handler = util.Logger(jwtauth.AuthMiddleware(VarsCheckMiddleware(handler)), route.Name)
+			handler = util.Logger(jwtauth.AuthMiddleware(VarsCheckMiddleware(AllowCORSHeader(handler))), route.Name)
+			fakeHandler = util.Logger(jwtauth.AuthMiddleware(VarsCheckMiddleware(AllowCORSHeader(fakeHandler))), fakeRoute.Name)
 		}
 
 		router.
@@ -32,6 +42,12 @@ func NewRouter() *mux.Router {
 			Handler(handler).
 			Queries(route.Queries...)
 
+		router.
+			Methods(fakeRoute.Method).
+			Path(fakeRoute.Pattern).
+			Name(fakeRoute.Name + " " + route.Name).
+			Handler(fakeHandler).
+			Queries(route.Queries...)
 	}
 
 	return router
@@ -48,4 +64,19 @@ func VarsCheckMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 
+}
+
+func AllowCORSHeader(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, Access-Control-Allow-Origin")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func corsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
 }
